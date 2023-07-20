088730eeb25dd2a1b8752de4d99508821babab7c
Sergey <sergey.ose.pyatigorsk@gmail.com>
2023-07-20 21:02:42 +0300

    feat: add styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { createRoot } from 'react-dom/client'
 
 import { App } from './App.tsx'
-
-import './index.css'
+import './styles/index.scss'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
 
 createRoot(document.getElementById('root') as HTMLElement).render(<App />)
