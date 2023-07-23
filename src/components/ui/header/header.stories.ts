fca6a1530f5611586dd856f53cb4ed992ec1149e
vladnen97 <darkdescent1991@gmail.com>
2023-07-23 13:59:50 +0300

    fix: delete stories

--- src/components/ui/header/header.stories.ts
+++ src/components/ui/header/header.stories.ts
@@ -17,49 +17,10 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Primary: Story = {
+export const TEST1HEADER: Story = {
   args: {
     variant: 'primary',
     children: 'Primary Button',
     disabled: false,
-  },
-}
-
-export const Secondary: Story = {
-  args: {
-    variant: 'secondary',
-    children: 'Secondary Button',
-    disabled: false,
-  },
-}
-export const Tertiary: Story = {
-  args: {
-    variant: 'tertiary',
-    children: 'Tertiary Button',
-    disabled: false,
-  },
-}
-export const Link: Story = {
-  args: {
-    variant: 'link',
-    children: 'Tertiary Button',
-    disabled: false,
-  },
-}
-
-export const FullWidth: Story = {
-  args: {
-    variant: 'primary',
-    children: 'Full Width Button',
-    disabled: false,
-    fullWidth: true,
-  },
-}
-
-export const AsLink: Story = {
-  args: {
-    variant: 'primary',
-    children: 'Link that looks like a button',
-    as: 'a',
   },
 }
