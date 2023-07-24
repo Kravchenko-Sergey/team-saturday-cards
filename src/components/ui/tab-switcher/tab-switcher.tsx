7c43af540e6e6fba3502eca68f503224c0f2b741
Sergey <sergey.ose.pyatigorsk@gmail.com>
2023-07-24 19:59:16 +0300

    feat: add tab-switcher.tsx

---
+++ src/components/ui/tab-switcher/tab-switcher.tsx
@@ -0,0 +1,45 @@
+import { FC, ReactNode } from 'react'
+
+import * as Tabs from '@radix-ui/react-tabs'
+
+import s from './tabSwitcher.module.scss'
+
+import { Typography } from 'components/ui/typography'
+
+type TabSwitcherProps = {
+  title: string
+  children: ReactNode
+}
+
+export const TabSwitcher: FC<TabSwitcherProps> = ({ title, children }) => {
+  return (
+    <div>
+      <Typography variant="body2" className={s.title}>
+        {title}
+      </Typography>
+      <Tabs.Root className={s.tabsRoot} defaultValue="tab1">
+        <Tabs.List className={s.tabsList}>{children}</Tabs.List>
+      </Tabs.Root>
+    </div>
+  )
+}
+
+type TabSwitcherItemProps = {
+  children: ReactNode
+  value: string
+  className: string
+  onClick?: () => void
+}
+
+export const TabSwitcherItem: FC<TabSwitcherItemProps> = ({
+  children,
+  value,
+  className,
+  onClick,
+}) => {
+  return (
+    <Tabs.Trigger value={value} className={className} onClick={onClick}>
+      {children}
+    </Tabs.Trigger>
+  )
+}
