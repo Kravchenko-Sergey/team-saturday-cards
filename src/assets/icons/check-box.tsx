273b925c2d9af35ff37b5f0e0c6b50c825e89d67
vladnen97 <darkdescent1991@gmail.com>
2023-07-25 18:28:07 +0300

    fix: change attributes

--- src/assets/icons/check-box.tsx
+++ src/assets/icons/check-box.tsx
@@ -1,10 +1,18 @@
 import { SVGProps, Ref, forwardRef, memo } from 'react'
+
 const SvgComponent = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="none" ref={ref} {...props}>
-    <path fill="var(--color-dark-900)" d="M4 6h16v12H4z" />
+  <svg
+    width={18}
+    height={18}
+    viewBox="0 0 18 18"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    ref={ref}
+    {...props}
+  >
     <path
-      fill="var(--color-light-100)"
-      d="M19 3H5a2 2 0 0 0-2 2v14c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2Zm-9 14-5-5 1.4-1.4 3.6 3.6 7.6-7.6L19 8l-9 9Z"
+      d="M16 0H2a2 2 0 00-2 2v14a2 2 0 002 2h14a2 2 0 002-2V2a2 2 0 00-2-2zM7 14L2 9l1.41-1.41L7 11.17l7.59-7.59L16 5l-9 9z"
+      fill="currentColor"
     />
   </svg>
 )
