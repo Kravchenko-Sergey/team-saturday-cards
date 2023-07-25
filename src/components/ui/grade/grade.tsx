47c2d201d4a17b1bb07e18c48a296f8592b99c85
Sergey <sergey.ose.pyatigorsk@gmail.com>
2023-07-25 12:24:28 +0300

    feat: add grade.tsx

---
+++ src/components/ui/grade/grade.tsx
@@ -0,0 +1,48 @@
+import { FC } from 'react'
+
+import { Star, StarOutlineOutline } from 'assets/icons'
+
+type RatingValue = 0 | 1 | 2 | 3 | 4 | 5
+
+type GradeProps = {
+  value: RatingValue
+  onClick: (value: RatingValue) => void
+}
+
+export const Grade: FC<GradeProps> = ({ value, onClick }) => {
+  return (
+    <div>
+      <StarItem value={1} onClick={onClick} selected={value > 0} />
+      <StarItem value={2} onClick={onClick} selected={value > 1} />
+      <StarItem value={3} onClick={onClick} selected={value > 2} />
+      <StarItem value={4} onClick={onClick} selected={value > 3} />
+      <StarItem value={5} onClick={onClick} selected={value > 4} />
+    </div>
+  )
+}
+
+type StarItemProps = {
+  value: RatingValue
+  selected: boolean
+  onClick: (value: RatingValue) => void
+}
+
+const StarItem: FC<StarItemProps> = ({ value, selected, onClick }) => {
+  return (
+    <div>
+      {selected ? (
+        <Star
+          onClick={() => {
+            onClick(value)
+          }}
+        />
+      ) : (
+        <StarOutlineOutline
+          onClick={() => {
+            onClick(value)
+          }}
+        />
+      )}
+    </div>
+  )
+}
