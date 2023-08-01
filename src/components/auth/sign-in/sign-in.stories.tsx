127d486de7a0988424625537f633bd628544134f
vladnen97 <darkdescent1991@gmail.com>
2023-08-01 18:39:59 +0300

    feat: add decorator

--- src/components/auth/sign-in/sign-in.stories.tsx
+++ src/components/auth/sign-in/sign-in.stories.tsx
@@ -1,10 +1,18 @@
 import type { Meta, StoryObj } from '@storybook/react'
+import { BrowserRouter } from 'react-router-dom'
 
 import { FormValues, SignIn } from './'
 
 const meta = {
   title: 'Components/AUTH/SignIn',
   component: SignIn,
+  decorators: [
+    Story => (
+      <BrowserRouter>
+        <Story />
+      </BrowserRouter>
+    ),
+  ],
   tags: ['autodocs'],
 } satisfies Meta<typeof SignIn>
 
