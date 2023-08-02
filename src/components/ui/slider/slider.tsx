25b5d61d62b18a16b26fe609c9e8bc345df2af7f
Sergey <sergey.ose.pyatigorsk@gmail.com>
2023-08-02 12:31:25 +0300

    fix: added the ability to enter slider values in inputs

--- src/components/ui/slider/slider.tsx
+++ src/components/ui/slider/slider.tsx
@@ -4,8 +4,6 @@ import * as SliderRadix from '@radix-ui/react-slider'
 
 import s from './slider.module.scss'
 
-import { Typography } from 'components/ui/typography'
-
 type SliderProps = SliderRadix.SliderProps & {
   multiple?: boolean
   value: number[]
@@ -17,17 +15,30 @@ export const Slider: FC<SliderProps> = ({
   onValueCommit,
   multiple,
   disabled,
-  min,
-  max,
-  step,
+  min = 0,
+  max = 100,
+  step = 1,
 }) => {
   return (
     <div className={disabled ? s.disabled : s.container}>
-      {value.length !== 1 && <Typography className={s.value}>{value[0]}</Typography>}
+      {value.length !== 1 && (
+        <input
+          type="number"
+          value={value[0]}
+          onChange={e => {
+            if (Number(e.target.value) > max) return
+            onValueChange!([Number(e.target.value), value[1]])
+            e.target.value = String(Number(e.target.value))
+          }}
+          className={s.value}
+        />
+      )}
       <SliderRadix.Root
         className={s.root}
         value={value}
-        onValueChange={onValueChange}
+        onValueChange={newValue => {
+          onValueChange!(newValue)
+        }}
         onValueCommit={onValueCommit}
         disabled={disabled}
         min={min}
@@ -40,7 +51,16 @@ export const Slider: FC<SliderProps> = ({
         <SliderRadix.Thumb className={s.thumb} />
         {multiple && <SliderRadix.Thumb className={s.thumb} />}
       </SliderRadix.Root>
-      <Typography className={s.value}>{value.length === 1 ? value[0] : value[1]}</Typography>
+      <input
+        type="number"
+        value={value.length === 1 ? value[0] : value[1]}
+        onChange={e => {
+          if (Number(e.target.value) > max) return
+          onValueChange!([value[0], Number(e.target.value)])
+          e.target.value = String(Number(e.target.value))
+        }}
+        className={s.value}
+      />
     </div>
   )
 }
