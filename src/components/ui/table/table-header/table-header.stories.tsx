853281fc9e0b2f4a887eaa97a5635ff94d1a3bf3
Sergey <sergey.ose.pyatigorsk@gmail.com>
2023-08-06 18:23:07 +0300

    feat: add table-header.stories.tsx

---
+++ src/components/ui/table/table-header/table-header.stories.tsx
@@ -0,0 +1,103 @@
+import { useState } from 'react'
+
+import { Meta } from '@storybook/react'
+
+import { Decks } from '@/pages/decks/decks.tsx'
+import { Column, Sort, TableHeader } from 'components/ui/table/table-header'
+
+const meta = {
+  title: 'Pages/Decks',
+  component: Decks,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Decks>
+
+const data = [
+  {
+    title: 'Project A',
+    cardsCount: 10,
+    updated: '2023-07-07',
+    createdBy: 'John Doe',
+  },
+  {
+    title: 'Project B',
+    cardsCount: 5,
+    updated: '2023-07-06',
+    createdBy: 'Jane Smith',
+  },
+  {
+    title: 'Project C',
+    cardsCount: 8,
+    updated: '2023-07-05',
+    createdBy: 'Alice Johnson',
+  },
+  {
+    title: 'Project D',
+    cardsCount: 3,
+    updated: '2023-07-07',
+    createdBy: 'Bob Anderson',
+  },
+  {
+    title: 'Project E',
+    cardsCount: 12,
+    updated: '2023-07-04',
+    createdBy: 'Emma Davis',
+  },
+]
+
+export const WithSort = {
+  render: () => {
+    const [sort, setSort] = useState<Sort>(null)
+
+    const sortString = sort ? `${sort.key}-${sort.direction}` : null
+
+    console.log(sortString)
+
+    const columns: Column[] = [
+      {
+        key: 'name',
+        title: 'Name',
+        isSortable: true,
+      },
+      {
+        key: 'cardsCount',
+        title: 'Cards',
+        isSortable: true,
+      },
+      {
+        key: 'updated',
+        title: 'Last Updated',
+        isSortable: true,
+      },
+      {
+        key: 'createdBy',
+        title: 'Created by',
+        isSortable: true,
+      },
+      {
+        key: 'options',
+        title: '',
+      },
+    ]
+
+    console.log(sort)
+
+    return (
+      <table>
+        <TableHeader columns={columns} onSort={setSort} sort={sort} />
+        <tbody>
+          {data.map(item => (
+            <tr key={item.title}>
+              <td>{item.title}</td>
+              <td>{item.cardsCount}</td>
+              <td>{item.updated}</td>
+              <td>{item.createdBy}</td>
+              <td>icons...</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    )
+  },
+}
+
+export default meta
