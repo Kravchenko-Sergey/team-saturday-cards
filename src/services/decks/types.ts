8ca57abdcac3a3d81212a8d640af1a250bfd0d61
Sergey <sergey.ose.pyatigorsk@gmail.com>
2023-08-08 18:31:58 +0300

    feat: add types

--- src/services/decks/types.ts
+++ src/services/decks/types.ts
@@ -1,3 +1,5 @@
+export type Deck = {}
+
 export type Author = {
   id: string
   name: string
@@ -25,44 +27,4 @@ export type ArgCreateDeck = {
   isPrivate: boolean
 }
 
-export type DeleteDeckResponse = {
-  id: string
-  userId: string
-  name: string
-  isPrivate: boolean
-  shots: number
-  cover?: any
-  rating: number
-  isDeleted?: any
-  isBlocked?: any
-  created: string
-  updated: string
-  cardsCount: number
-}
-
-export type Pagination = {
-  totalPages: number
-  currentPage: number
-  itemsPerPage: number
-  totalItems: number
-}
-
-export type Items = {
-  id: string
-  question: string
-  answer: string
-  deckId: string
-  questionImg?: any
-  answerImg?: any
-  questionVideo?: any
-  answerVideo?: any
-  created: string
-  updated: string
-  shots: number
-  grade: number
-}
-
-export type GetCardsResponse = {
-  pagination: Pagination
-  items: Items[]
-}
+export type DeleteDeckResponse = Omit<CreateDeckResponse, 'author'>
