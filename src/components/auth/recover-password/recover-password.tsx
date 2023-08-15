24c555de244e40bc46138b13d782f3906a594a48
Sergey <sergey.ose.pyatigorsk@gmail.com>
2023-08-15 23:40:21 +0300

    feat: add links to pages

--- src/components/auth/recover-password/recover-password.tsx
+++ src/components/auth/recover-password/recover-password.tsx
@@ -1,6 +1,6 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 import { FormValues, recoverPasswordSchema } from './'
 
@@ -13,10 +13,12 @@ import { Typography } from 'components/ui/typography'
 import s from './recover-password.module.scss'
 
 type Props = {
-  onSubmit: (data: FormValues) => void
+  onSubmit: (data: { html: string; email: string }) => any
 }
 export const RecoverPassword = (props: Props) => {
-  const { handleSubmit, control } = useForm<FormValues>({
+  const navigate = useNavigate()
+
+  const { handleSubmit, control, getValues } = useForm<FormValues>({
     mode: 'onSubmit',
     resolver: zodResolver(recoverPasswordSchema),
     defaultValues: {
@@ -24,12 +26,24 @@ export const RecoverPassword = (props: Props) => {
     },
   })
 
+  const onSubmit = () => {
+    props
+      .onSubmit({
+        html: "<h1>Hi, ##name##</h1><p>Click <a href='http://localhost:5173/set-new-password/##token##'>here</a> to recover your password</p><p>'##token##'</p>>",
+        email: getValues().email,
+      })
+      .unwrap()
+      .then(() => {
+        navigate('/check-email')
+      })
+  }
+
   return (
     <Card className={s.card}>
       <Typography variant={'large'} as={'h1'} className={s.title}>
         Forgot your password?
       </Typography>
-      <form onSubmit={handleSubmit(props.onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <ControlledTextField name={'email'} label={'Email'} type={'email'} control={control} />
         <Typography variant={'body2'} as={'div'} className={s.description}>
           Enter your email address and we will send you further instructions
