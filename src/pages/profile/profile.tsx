2b91820050cbfed9109ef1e6e1a0617886a461cf
Sergey <sergey.ose.pyatigorsk@gmail.com>
2023-08-16 16:14:45 +0300

    feat: added registration and profile change requests

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,31 +1,25 @@
-import { useState } from 'react'
-
-import { useMeQuery } from '@/services/auth/auth.api.ts'
+import {
+  useChangeProfileMutation,
+  useLogoutMutation,
+  useMeQuery,
+} from '@/services/auth/auth.api.ts'
 import { ProfileInfo } from 'components/auth/profile-info'
 
 export const Profile = () => {
-  const [showTextField, setShowTextField] = useState(false)
-
   const { data } = useMeQuery()
+  const [logout] = useLogoutMutation()
+  const [changeProfile] = useChangeProfileMutation()
 
   return (
-    <>
-      <ProfileInfo
-        name={data.name}
-        email={data.email}
-        src={
-          'https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png'
-        }
-        handleChangeName={() => {
-          setShowTextField(true)
-        }}
-        handleChangeAvatar={() => {}}
-        showTextField={showTextField}
-        onSubmit={() => {
-          setShowTextField(false)
-        }}
-        handleLogout={() => {}}
-      />
-    </>
+    <ProfileInfo
+      name={data.name}
+      email={data.email}
+      src={
+        'https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png'
+      }
+      handleChangeAvatar={() => {}}
+      onSubmit={changeProfile}
+      handleLogout={() => logout({})}
+    />
   )
 }
