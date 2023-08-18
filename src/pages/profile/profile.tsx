0c395c63027b1facb26c7bf2a735b00307396bd8
Sergey <sergey.ose.pyatigorsk@gmail.com>
2023-08-18 17:55:03 +0300

    feat: added change avatar

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,3 +1,5 @@
+import { ChangeEvent, useState } from 'react'
+
 import {
   useChangeProfileMutation,
   useLogoutMutation,
@@ -6,20 +8,46 @@ import {
 import { ProfileInfo } from 'components/auth/profile-info'
 
 export const Profile = () => {
-  const { data } = useMeQuery()
+  const [showTextField, setShowTextField] = useState(false)
+  const [ava, setAva] = useState<File | null>(null)
+
+  const { currentData } = useMeQuery()
   const [logout] = useLogoutMutation()
   const [changeProfile] = useChangeProfileMutation()
 
+  const handleFormSubmit = (data: { email: string; name: string }) => {
+    setShowTextField(false)
+
+    const form = new FormData()
+
+    form.append('email', currentData.email)
+    form.append('name', data.name)
+    ava && form.append('avatar', ava)
+
+    changeProfile(form)
+  }
+
+  const handleChangeAvatar = (e: ChangeEvent<HTMLInputElement>) => {
+    setShowTextField(true)
+    const file = e.target.files![0]
+
+    setAva(file)
+  }
+
   return (
     <ProfileInfo
-      name={data.name}
-      email={data.email}
+      name={currentData.name}
+      email={currentData.email}
       src={
-        'https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png'
+        currentData
+          ? currentData.avatar
+          : 'https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png'
       }
-      handleChangeAvatar={() => {}}
-      onSubmit={changeProfile}
+      handleChangeAvatar={handleChangeAvatar}
+      onSubmit={handleFormSubmit}
       handleLogout={() => logout({})}
+      showTextField={showTextField}
+      setShowTextField={setShowTextField}
     />
   )
 }
